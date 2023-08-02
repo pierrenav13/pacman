1d94ba383e628644c0eca1314b904c0ebe632ea9
pierre <pnavarin@gmail.com>
2023-08-02 13:10:01 -0400

    first level

--- pacman.py
+++ pacman.py
@@ -1,10 +1,30 @@
 import pygame
+from board import boards
 
 pygame.init()
 
+#Global variables for h * w
 WIDTH = 900
 HEIGHT = 950
 
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
-time = pygame.time.Clock()
+timer = pygame.time.Clock()
 fps = 60
+font = pygame.font.Font('freesansbold.ttf', 20)
+active_level = 0
+level = boards[active_level]
+
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill('black')
+    draw_board()
+
+    #condition to exit the loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    #
+    pygame.display.flip()
+pygame.quit()
