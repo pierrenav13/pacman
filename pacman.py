984c1eb7c3fe93d5543c0e0845b56ccdd6dec859
pierre <pnavarin@gmail.com>
2023-08-04 13:50:08 -0400

    animated character + direction

--- pacman.py
+++ pacman.py
@@ -5,6 +5,7 @@ import math
 pygame.init()
 
 #Global variables for h * w
+#Dimensions and powerup size multiplied by .8 for personal screen size
 WIDTH = 900 * .8
 HEIGHT = 950 * .8
 
@@ -16,8 +17,16 @@ active_level = 0
 level = boards[active_level]
 color = 'blue'
 PI = math.pi
+player_images = []
+#using *.8 for personal screen size
+for i in range(1, 5):
+    player_images.append(pygame.transform.scale(pygame.image.load(f'assets/player_images/{i}.png'), (38, 38)))
+player_x = 335
+player_y = 520
+direction = 0
+counter = 0
 
-def draw_board(l):
+def draw_board():
     #height and width of each tile/piece
     num1 = ((HEIGHT - 50) // 32)
     num2 = (WIDTH // 30)
@@ -47,18 +56,43 @@ def draw_board(l):
             if level[i][j] == 9:
                 pygame.draw.line(screen, 'white', ((j * num2, (0.5*num1) + i * num1)),  (j * num2 + num2, i * num1 + (0.5*num1)), 3)
 
+def draw_player():
+    #Right-0, Left-1, Up-2, Down-3
+    if direction == 0:
+        screen.blit(player_images[counter // 5], (player_x, player_y))
+    if direction == 1:
+        screen.blit(pygame.transform.flip(player_images[counter // 5], True, False), (player_x, player_y))
+    if direction == 2:
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 90), (player_x, player_y))
+    if direction == 3:
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 270), (player_x, player_y))
+    
 
 run = True
 while run:
     timer.tick(fps)
+    if counter < 19:
+        counter += 1
+    else:
+        counter = 0
+
     screen.fill('black')
-    draw_board(level)
+    draw_board()
+    draw_player()
 
     #condition to exit the loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    
-    #
+        if event.type == pygame.KEYDOWN:
+            if event.key == (pygame.K_d):
+                direction = 0
+            if event.key == (pygame.K_a):
+                direction = 1
+            if event.key == (pygame.K_w):
+                direction = 2
+            if event.key == (pygame.K_s):
+                direction = 3
+
     pygame.display.flip()
 pygame.quit()
