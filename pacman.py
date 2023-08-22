6a4b0a0e8375be1d3dafa7f9b988b1bab383247f
pierre <pnavarin@gmail.com>
2023-08-22 09:36:42 -0400

    Project finished

--- pacman.py
+++ pacman.py
@@ -1,6 +1,6 @@
-import pygame
 import copy
 from board import boards
+import pygame
 import math
 
 pygame.init()
@@ -12,11 +12,10 @@ timer = pygame.time.Clock()
 fps = 60
 font = pygame.font.Font('freesansbold.ttf', 20)
 active_level = 0
-level = boards[active_level]
+level = copy.deepcopy(boards[active_level])
 color = 'blue'
 PI = math.pi
 player_images = []
-#using *.8 for personal screen size
 for i in range(1, 5):
     player_images.append(pygame.transform.scale(pygame.image.load(f'assets/player_images/{i}.png'), (45, 45)))
 blinky_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/red.png'), (45, 45))
@@ -42,7 +41,7 @@ clyde_y = 438
 clyde_direction = 2
 counter = 0
 flicker = False
-#Right, Left, Up, Down
+# R, L, U, D
 turns_allowed = [False, False, False, False]
 direction_command = 0
 player_speed = 2
@@ -65,6 +64,7 @@ startup_counter = 0
 lives = 3
 game_over = False
 game_won = False
+
 
 class Ghost:
     def __init__(self, x_coord, y_coord, target, speed, img, direct, dead, box, id):
@@ -658,6 +658,7 @@ class Ghost:
             self.x_pos - 30
         return self.x_pos, self.y_pos, self.direction
 
+
 def draw_misc():
     score_text = font.render(f'Score: {score}', True, 'white')
     screen.blit(score_text, (10, 920))
@@ -676,6 +677,7 @@ def draw_misc():
         gameover_text = font.render('Victory! Space bar to restart!', True, 'green')
         screen.blit(gameover_text, (100, 300))
 
+
 def check_collisions(scor, power, power_count, eaten_ghosts):
     num1 = (HEIGHT - 50) // 32
     num2 = WIDTH // 30
@@ -691,56 +693,59 @@ def check_collisions(scor, power, power_count, eaten_ghosts):
             eaten_ghosts = [False, False, False, False]
     return scor, power, power_count, eaten_ghosts
 
+
 def draw_board():
-    #height and width of each tile/piece
     num1 = ((HEIGHT - 50) // 32)
     num2 = (WIDTH // 30)
-    #iterate through each row in the level
     for i in range(len(level)):
-        #iterate through each column for the row 
         for j in range(len(level[i])):
-            #1 small circle, 2 powerup
             if level[i][j] == 1:
-                pygame.draw.circle(screen, 'white', (j * num2 + (0.5*num2), i * num1 + (0.5 * num1)), 4)
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
             if level[i][j] == 2 and not flicker:
-                pygame.draw.circle(screen, 'white', (j * num2 + (0.5*num2), i * num1 + (0.5 * num1)), 10)
-            #3 Vertical, 4 Horizontal
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
             if level[i][j] == 3:
-                pygame.draw.line(screen, color, ((j * num2 + (0.5*num2), i * num1)),  (j * num2 + (0.5*num2), i * num1 + num1), 3)
+                pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
+                                 (j * num2 + (0.5 * num2), i * num1 + num1), 3)
             if level[i][j] == 4:
-                pygame.draw.line(screen, color, ((j * num2, (0.5*num1) + i * num1)),  (j * num2 + num2, i * num1 + (0.5*num1)), 3)
-            #5-8 Corners, 9 Ghost Gate
+                pygame.draw.line(screen, color, (j * num2, i * num1 + (0.5 * num1)),
+                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
             if level[i][j] == 5:
-                pygame.draw.arc(screen, color, [(j*num2 - (num2*0.4)) - 2, (i * num1 + (0.5*num1)), num2, num1], 0, PI/2, 3)
+                pygame.draw.arc(screen, color, [(j * num2 - (num2 * 0.4)) - 2, (i * num1 + (0.5 * num1)), num2, num1],
+                                0, PI / 2, 3)
             if level[i][j] == 6:
-                pygame.draw.arc(screen, color, [(j*num2 + (num2*0.5)), (i * num1 + (0.5*num1)), num2, num1], PI/2, PI, 3)
+                pygame.draw.arc(screen, color,
+                                [(j * num2 + (num2 * 0.5)), (i * num1 + (0.5 * num1)), num2, num1], PI / 2, PI, 3)
             if level[i][j] == 7:
-                pygame.draw.arc(screen, color, [(j*num2 + (num2*0.5)), (i * num1 - (0.4*num1)), num2, num1], PI, (3*PI)/2, 3)
+                pygame.draw.arc(screen, color, [(j * num2 + (num2 * 0.5)), (i * num1 - (0.4 * num1)), num2, num1], PI,
+                                3 * PI / 2, 3)
             if level[i][j] == 8:
-                pygame.draw.arc(screen, color, [(j*num2 - (num2*0.4)) - 2, (i * num1 - (0.4*num1)), num2, num1], (3*PI)/2, (2*PI), 3)
+                pygame.draw.arc(screen, color,
+                                [(j * num2 - (num2 * 0.4)) - 2, (i * num1 - (0.4 * num1)), num2, num1], 3 * PI / 2,
+                                2 * PI, 3)
             if level[i][j] == 9:
-                pygame.draw.line(screen, 'white', ((j * num2, (0.5*num1) + i * num1)),  (j * num2 + num2, i * num1 + (0.5*num1)), 3)
+                pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
+                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+
 
 def draw_player():
-    #Right-0, Left-1, Up-2, Down-3
+    # 0-RIGHT, 1-LEFT, 2-UP, 3-DOWN
     if direction == 0:
         screen.blit(player_images[counter // 5], (player_x, player_y))
-    if direction == 1:
+    elif direction == 1:
         screen.blit(pygame.transform.flip(player_images[counter // 5], True, False), (player_x, player_y))
-    if direction == 2:
+    elif direction == 2:
         screen.blit(pygame.transform.rotate(player_images[counter // 5], 90), (player_x, player_y))
-    if direction == 3:
+    elif direction == 3:
         screen.blit(pygame.transform.rotate(player_images[counter // 5], 270), (player_x, player_y))
-    
+
+
 def check_position(centerx, centery):
     turns = [False, False, False, False]
     num1 = (HEIGHT - 50) // 32
     num2 = (WIDTH // 30)
-    #multiplication by .8 may be neccessary
     num3 = 15
-    #check collisions based on center x and y of player +/- num3
+    # check collisions based on center x and center y of player +/- fudge number
     if centerx // 30 < 29:
-        #Checking if going in the opposite direction is possible
         if direction == 0:
             if level[centery // num1][(centerx - num3) // num2] < 3:
                 turns[1] = True
@@ -754,7 +759,6 @@ def check_position(centerx, centery):
             if level[(centery - num3) // num1][centerx // num2] < 3:
                 turns[2] = True
 
-        #May need to multiply 12 and 18 by .8 due to previous adjustment
         if direction == 2 or direction == 3:
             if 12 <= centerx % num2 <= 18:
                 if level[(centery + num3) // num1][centerx // num2] < 3:
@@ -766,7 +770,6 @@ def check_position(centerx, centery):
                     turns[1] = True
                 if level[centery // num1][(centerx + num2) // num2] < 3:
                     turns[0] = True
-
         if direction == 0 or direction == 1:
             if 12 <= centerx % num2 <= 18:
                 if level[(centery + num1) // num1][centerx // num2] < 3:
@@ -778,17 +781,15 @@ def check_position(centerx, centery):
                     turns[1] = True
                 if level[centery // num1][(centerx + num3) // num2] < 3:
                     turns[0] = True
-
-
-
     else:
         turns[0] = True
         turns[1] = True
 
     return turns
 
+
 def move_player(play_x, play_y):
-    #R L U D
+    # r, l, u, d
     if direction == 0 and turns_allowed[0]:
         play_x += player_speed
     elif direction == 1 and turns_allowed[1]:
@@ -798,6 +799,7 @@ def move_player(play_x, play_y):
     elif direction == 3 and turns_allowed[3]:
         play_y += player_speed
     return play_x, play_y
+
 
 def get_targets(blink_x, blink_y, ink_x, ink_y, pink_x, pink_y, clyd_x, clyd_y):
     if player_x < 450:
@@ -888,7 +890,6 @@ while run:
     else:
         counter = 0
         flicker = True
-
     if powerup and power_counter < 600:
         power_counter += 1
     elif powerup and power_counter >= 600:
@@ -903,10 +904,8 @@ while run:
 
     screen.fill('black')
     draw_board()
-    draw_player()
     center_x = player_x + 23
     center_y = player_y + 24
-
     if powerup:
         ghost_speeds = [1, 1, 1, 1]
     else:
@@ -928,7 +927,6 @@ while run:
     if clyde_dead:
         ghost_speeds[3] = 4
 
-
     game_won = True
     for i in range(len(level)):
         if 1 in level[i] or 2 in level[i]:
@@ -947,10 +945,7 @@ while run:
     draw_misc()
     targets = get_targets(blinky_x, blinky_y, inky_x, inky_y, pinky_x, pinky_y, clyde_x, clyde_y)
 
-    
     turns_allowed = check_position(center_x, center_y)
-    #player_x, player_y = move_player(player_x, player_y)
-
     if moving:
         player_x, player_y = move_player(player_x, player_y)
         if not blinky_dead and not blinky.in_box:
@@ -1144,8 +1139,6 @@ while run:
         eaten_ghost[3] = True
         score += (2 ** eaten_ghost.count(True)) * 100
 
-
-    #condition to exit the loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -1189,6 +1182,7 @@ while run:
                 level = copy.deepcopy(boards)
                 game_over = False
                 game_won = False
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT and direction_command == 0:
                 direction_command = direction
@@ -1199,15 +1193,28 @@ while run:
             if event.key == pygame.K_DOWN and direction_command == 3:
                 direction_command = direction
 
+    if direction_command == 0 and turns_allowed[0]:
+        direction = 0
+    if direction_command == 1 and turns_allowed[1]:
+        direction = 1
+    if direction_command == 2 and turns_allowed[2]:
+        direction = 2
+    if direction_command == 3 and turns_allowed[3]:
+        direction = 3
 
-        for i in range(4):
-            if direction_command == i and turns_allowed[i]:
-                direction = i
+    if player_x > 900:
+        player_x = -47
+    elif player_x < -50:
+        player_x = 897
 
-        if player_x > 900:
-            player_x = -47
-        elif player_x < -50:
-            player_x = 897
+    if blinky.in_box and blinky_dead:
+        blinky_dead = False
+    if inky.in_box and inky_dead:
+        inky_dead = False
+    if pinky.in_box and pinky_dead:
+        pinky_dead = False
+    if clyde.in_box and clyde_dead:
+        clyde_dead = False
 
     pygame.display.flip()
 pygame.quit()
